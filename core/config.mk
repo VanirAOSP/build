fef4b3b7ee3b26e9803dfa5db575ff36be2d370a
PrimeDirective <activethrasher00@gmail.com>
2012-12-30 07:35:20 -0500

    build update for GCC4.7 from le goog via AOKP
    CS2: Revert unreleated signapk "build optimization" because not all apks are going to be squished...
    especially using tools like mm to compile a single apk for testing
    
    Squashed commit of the following:
    
    commit 124e0284c5a4978e8220f0d8452e5384cb37a662
    Author: Ben Cheng <bccheng@google.com>
    Date:   Mon Dec 10 16:04:39 2012 -0800
    
        Export TARGET_GCC_VERSION as an env variable.
    
        Have it referenced by the adbs tool to pick the specified toolchain.
    
        Change-Id: I23df88946254a55d0749d6753e54559faf33e121
    
    commit 55cbf3fadfaa4076d323ebd16309d861368bd358
    Author: aaronpoweruser <atlrudeboi1@gmail.com>
    Date:   Mon Dec 10 10:52:02 2012 -0500
    
        Use GCC 4.7 from google
    
        Noticible preformance improvement
        Fixes needed
        *Framework/rs
        *System/security
        *external/oprofile
        *external/elfutils
        *external/v8
    
        Change-Id: Ic1eadf1fa7df1525471b9c844ac0c4bd9e778330
    
    commit 9b127a27a79a8eb3dcb5fc26a9e4dc34702021df
    Author: Ben Cheng <bccheng@google.com>
    Date:   Fri Nov 16 13:29:13 2012 -0800
    
        [ARM] Extend the usage of TARGET_GCC_VERSION for gdb and arm-eabi-*.
    
        In the future to upgrade the gcc version we just need to edit one
        line in core/config.mk.
    
        Change-Id: I01bb38c38f2388913bb7369aa4b2da480411dfbe
    
        Conflicts:
        	envsetup.sh
    
        Conflicts:
    
        	envsetup.sh
    
    commit 7b3f12961f5b7c52385ce793791154a3b435d661
    Author: Andrew Hsieh <andrewhsieh@google.com>
    Date:   Tue Oct 23 12:06:01 2012 +0800
    
        Add TARGET_GCC_VERSION
    
        Default to 4.6, but can overwritten to something else in
        prebuilts/gcc/$os/$arch/$arch-linux-android*-$ver
    
        Change-Id: I16f99d642b8ff8e831d392261a0c536041107051
    
    Change-Id: I2cde7c661fe1c3cb98d06a81ad7ea18e54c8a10d

--- core/config.mk
+++ core/config.mk
@@ -225,6 +225,9 @@ ifeq ($(TARGET_CPU_ABI),)
 endif
 TARGET_CPU_ABI2 := $(strip $(TARGET_CPU_ABI2))
 
+# default target GCC version
+TARGET_GCC_VERSION := 4.7
+
 # $(1): os/arch
 define select-android-config-h
 build/core/combo/include/arch/$(1)/AndroidConfig.h
