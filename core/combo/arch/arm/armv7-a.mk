794bb15fda09f0b1400b0697acbc5586021071dd
PrimeDirective <activethrasher00@gmail.com>
2013-02-24 03:53:44 -0500

    more precision in ARMv7-a flags
    Change-Id: I72d6cd0a8c495bb500187a91ba01cb3d2ea561ed

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -11,7 +11,6 @@ ARCH_ARM_HAVE_VFP               := true
 #
 arch_variant_cflags := \
     -march=armv7-a \
-    -mfloat-abi=softfp \
     -mfpu=vfpv3-d16
 
 ifneq (,$(findstring cpu=cortex-a9,$(TARGET_EXTRA_CFLAGS)))
@@ -25,4 +24,14 @@ endif
 ifneq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
 arch_variant_cflags += \
     -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
+endif
+
+ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
+arch_variant_cflags += \
+  -mfloat-abi=softfp \
+  -mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
+else
+# fall back on soft tunning if fpu is not specified
+arch_variant_cflags += \
+  -mfloat-abi=soft
 endif
