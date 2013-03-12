13a9fc8c361551ee705ffcd08cbbf52f1ec0257d
PrimeDirective <activethrasher00@gmail.com>
2013-03-12 00:58:31 -0400

    Add specifics to armv7-a and armv7-a-neon
    make sure build is compiling with mcpu march mtune. also
    use ARCH_ARM_USE_D16 for VFP and add basic NEON fpu.
    
    Change-Id: I2da6c1938eaddb303c931064a1f7067e4ca1c8e4

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -5,24 +5,46 @@ ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
-ARCH_ARM_HAVE_VFP_D32           := true
-ARCH_ARM_HAVE_NEON              := true
-
-
-ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
-	arch_variant_cflags := -mcpu=cortex-a15
+# retain AOSP convention for ARCH_ARM_VFP_D32
+# within build by appending to device tree to modify
+ifeq ($(ARCH_ARM_USE_D16),true)
+ARCH_ARM_HAVE_VFP_D32           := false
 else
-	arch_variant_cflags := -march=armv7-a
+ARCH_ARM_HAVE_VFP_D32           := true
+endif
+ARCH_ARM_HAVE_NEON              := true
+ifeq ($(strip $(TARGET_CPU_SMP)),true)
+ARCH_ARM_HAVE_TLS_REGISTER      := true
 endif
 
-ifneq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
+# define the defaults
+arch_variant_cflags := \
+     -mcpu=$(strip $(TARGET_ARCH_VARIANT_CPU)) \
+     -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU)) \
+     -mfpu=neon \
+
+# append more specific neon and fpu if defined
+# todo: add more device specific soft/softfp/hard
+ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
 arch_variant_cflags += \
-    -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
+     -mfloat-abi=softfp \
+     -mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
+else
+arch_variant_cflags += \
+     -mfloat-abi=softfp
 endif
 
+ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a9)
 arch_variant_cflags += \
-    -mfloat-abi=softfp \
-    -mfpu=neon
+#    Remove -march=cortex-a15 until better support is provided. Not
+#    entirely sure it's necessary anyways since -mcpu implies -march.
+    -march=cortex-a9
+endif
+ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a15)
+arch_variant_cflags += \
+#    Remove -march=cortex-a15 until better support is provided.
+#    -march=cortex-a15
+endif
 
 ifneq (,$(findstring cpu=cortex-a9,$(TARGET_EXTRA_CFLAGS)))
 arch_variant_ldflags := \
@@ -33,18 +55,17 @@ arch_variant_ldflags := \
 endif
 
 #TODO: fine-tune generic values
-
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a15)
+ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a15)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 #ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
 endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a9)
+ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a9)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
 endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a8)
+ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a8)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -4,29 +4,37 @@
 ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
+ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),none)
 ARCH_ARM_HAVE_VFP               := true
+else
+ARCH_ARM_HAVE_VFP               := false
+endif
+ifeq ($(ARCH_ARM_USE_D16),true)
+ARCH_ARM_HAVE_VFP_D32           := false
+else
+ARCH_ARM_HAVE_VFP_D32           := true
+endif
+ifeq ($(strip $(TARGET_CPU_SMP)),true)
+ARCH_ARM_HAVE_TLS_REGISTER      := true
+endif
 
-# Note: Hard coding the 'tune' value here is probably not ideal,
+# Note: Hard coding the 'arch' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
 arch_variant_cflags := \
     -march=armv7-a \
-    -mfpu=vfpv3-d16
+    -mtune=$(TARGET_ARCH_VARIANT_CPU)
 
 ifneq (,$(findstring cpu=cortex-a9,$(TARGET_EXTRA_CFLAGS)))
-arch_variant_ldflags := \
+arch_variant_ldflags += \
 	-Wl,--no-fix-cortex-a8
 else
-arch_variant_ldflags := \
+arch_variant_ldflags += \
 	-Wl,--fix-cortex-a8
 endif
 
-ifneq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
-arch_variant_cflags += \
-    -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
-endif
-
-ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
+# if fpu is defined but not set use softfp and set fpu
+ifeq ($(strip $(ARCH_ARM_HAVE_VFP)),true)
 arch_variant_cflags += \
   -mfloat-abi=softfp \
   -mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
