bce75141b683dd04a23337cd1176c2f31bcf9905
nuclearmistake <nuclearmistake@gmail.com>
2013-09-06 08:52:17 -0400

    Restore live wallpapers and PhotoTable
    Change-Id: I1f750ade11c75ac45da3e274fbd04e160369ba6c

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -23,6 +23,17 @@ PRODUCT_PACKAGES := \
     libfwdlockengine \
     WAPPushManager
 
+PRODUCT_PACKAGES += \
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
+    VisualizationWallpapers \
+    PhotoTable
+
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy
