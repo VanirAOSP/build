35c03d15005e79d65c71b816cb0be66f198e479a
nuclearmistake <nuclearmistake@gmail.com>
2013-09-10 10:29:15 -0400

    Make cpu-specific optimizations simpler, smarter, and more compatible with CM
    infer what we use TARGET_ARCH_VARIANT_CPU for form TARGET_CPU_VARIANT
    if TARGET_ARCH_VARIANT_CPU is undefined (as is the case with a virgin CM
    device tree.
    (based on 720710b - build: Cleanup CPU-specific optimizations (4 weeks ago) <Steve Kondik>)
    
    assume some things are always true, and remove vestigial variables
    
    Change-Id: I7e5c5809b1dc3f19e99cb153461876077b237345

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -1,72 +1,77 @@
 # Configuration for Linux on ARM.
 # Generating binaries for the ARMv7-a architecture and higher with NEON
 #
-
-TARGET_ARCH_VARIANT_FPU := neon
-include $(BUILD_COMBOS)/arch/$(TARGET_ARCH)/armv7-a.mk
-
-ARCH_ARM_HAVE_CLZ               := true
-ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
-# retain AOSP convention for ARCH_ARM_VFP_D32
-# within build by appending to device tree to modify
-ifeq ($(ARCH_ARM_USE_D16),true)
-ARCH_ARM_HAVE_VFP_D32           := false
-else
 ARCH_ARM_HAVE_VFP_D32           := true
-endif
 ARCH_ARM_HAVE_NEON              := true
-ifeq ($(strip $(TARGET_CPU_SMP)),true)
-ARCH_ARM_HAVE_TLS_REGISTER      := true
-endif
 
 # is arch variant CPU defined?
 ifneq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
-
-ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a15)
-arch_cpu_without_ghosts := cortex-a9  #cortex-a15 has ghosts
+	cpu_for_optimizations := $(strip $(TARGET_ARCH_VARIANT_CPU))
 else
-arch_cpu_without_ghosts := $(strip $(TARGET_ARCH_VARIANT_CPU))
+# infer TARGET_ARCH_VARIANT_CPU from TARGET_CPU_VARIANT
+ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
+	cpu_for_optimizations := cortex-a15
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
+	cpu_for_optimizations := cortex-a9
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
+	cpu_for_optimizations := cortex-a7
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),krait)
+	cpu_for_optimizations := cortex-a9
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),scorpion)
+	cpu_for_optimizations := cortex-a8
+else
+	cpu_for_optimizations := armv7-a
 endif
-
-arch_variant_cflags += \
-	-mcpu=$(arch_cpu_without_ghosts) \
-	-mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
-
-else
-
-$(warn TARGET_ARCH_VARIANT_CPU is NOT SET! Using values from armv7-a.mk)
-
+endif
+endif
+endif
+endif
 endif #end of cpu stuff
 
-ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
-arch_variant_cflags += \
-	-mfloat-abi=softfp \
-	-mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
+ifneq ($(cpu_for_optimizations), armv7-a)
+ifeq ($(cpu_for_optimizations),cortex-a15)
+	arch_cpu_without_ghosts := cortex-a9  #cortex-a15 has ghosts
 else
-# fall back on soft tunning if fpu is not specified
-arch_variant_cflags += \
-	-mfloat-abi=soft
+	arch_cpu_without_ghosts := $(cpu_for_optimizations)
+endif
+arch_variant_cflags := \
+	-mcpu=$(arch_cpu_without_ghosts) \
+	-mtune=$(cpu_for_optimizations)
+else
+arch_variant_cflags := \
+	-march=armv7-a
 endif
 
+arch_variant_cflags += \
+	-mfloat-abi=softfp \
+	-mfpu=neon
+
+arch_variant_ldflags := \
+	-Wl,--fix-cortex-a8
+
 #TODO: fine-tune generic values
-ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a15)
+ifeq ($(cpu_for_optimizations),cortex-a15)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 #ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
 endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a9)
+ifeq ($(cpu_for_optimizations),cortex-a9)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
 endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a8)
+ifeq ($(cpu_for_optimizations),cortex-a8)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
 endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a5)
+ifeq ($(cpu_for_optimizations), cortex-a5)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
