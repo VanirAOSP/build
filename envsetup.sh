94d186ceadb7a6749c2e4acf4c016dfdd47c2448
Kyle Ladd <kyle@laddk.com>
2013-09-11 20:43:42 -0400

    build: fix bash completion sourcing
    Sourcing functions in files from a function within a file being
    sourced was giving bash a hard time. This fixes 'repo' command tab
    completions.
    
    Change-Id: Iac1b3078e20749fb474ed1270e0886cf435e24d9

--- envsetup.sh
+++ envsetup.sh
@@ -291,29 +291,19 @@ function settitle()
     fi
 }
 
-function addcompletions()
+function check_bash_version()
 {
     # Keep us from trying to run in something that isn't bash.
     if [ -z "${BASH_VERSION}" ]; then
-        return
+        return 1
     fi
 
     # Keep us from trying to run in bash that's too old.
     if [ "${BASH_VERSINFO[0]}" -lt 4 ] ; then
-        return
+        return 2
     fi
 
-    local T dir f
-
-    dirs="sdk/bash_completion vendor/cm/bash_completion"
-    for dir in $dirs; do
-    if [ -d ${dir} ]; then
-        for f in `/bin/ls ${dir}/[a-z]*.bash 2> /dev/null`; do
-            echo "including $f"
-            . $f
-        done
-    fi
-    done
+    return 0
 }
 
 function choosetype()
@@ -1635,7 +1625,18 @@ do
 done
 unset f
 
-addcompletions
+# Add completions
+check_bash_version && {
+    dirs="sdk/bash_completion vendor/cm/bash_completion"
+    for dir in $dirs; do
+    if [ -d ${dir} ]; then
+        for f in `/bin/ls ${dir}/[a-z]*.bash 2> /dev/null`; do
+            echo "including $f"
+            . $f
+        done
+    fi
+    done
+}
 
 #rst (repo start helper), rup (repo upload helper)
 source $(gettop)/build/nukehawtness
