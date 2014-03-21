e9fdbf03208f435774c0e01bac40ff7960cdac2a
Chris Heffley <tucstwo@gmail.com>
2014-03-20 21:47:25 -0400

    Make use of a QUIET repo repo conditional on existence of an environment variable
    Change-Id: I182592d6103f55eb88a8da53b35f18ede0e27091

--- envsetup.sh
+++ envsetup.sh
@@ -1728,13 +1728,14 @@ check_bash_version && {
     done
 }
 
-# SWITCH TO A VERSION OF REPO THAT DOESN'T SPAM PROJECT NAMES
-pushd . >& /dev/null
-cd $(gettop)/.repo/repo
-[ `git remote -v | grep vanir | wc -l` -eq 0 ] && git remote add vanir http://www.emccann.net/repo
-git fetch vanir >& /dev/null
-git checkout vanir/master >& /dev/null
-popd >& /dev/null
+if [ $STFU_REPO ]; then
+    pushd . >& /dev/null
+    cd $(gettop)/.repo/repo
+    [ `git remote -v | grep vanir | wc -l` -eq 0 ] && git remote add vanir http://www.emccann.net/repo
+    git fetch vanir >& /dev/null
+    git checkout vanir/master >& /dev/null
+    popd >& /dev/null
+fi
 
 #rst (repo start helper), rup (repo upload helper)
 source $(gettop)/build/nukehawtness
