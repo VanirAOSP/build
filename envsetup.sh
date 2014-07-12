62bd2f456e2a1256e8999deeb86aa9d17c8733a3
nuclearmistake <nuclearmistake@gmail.com>
2014-07-12 11:42:46 -0400

    Fix scripts that use the output of forall_vanir pwd, by not printing the pathlist too
    Change-Id: I770afd9c3d047b587a0625bf5605f0829bae2420

--- envsetup.sh
+++ envsetup.sh
@@ -63,7 +63,6 @@ function forall_vanir()
   for x in `cat $ANDROID_BUILD_TOP/.repo/manifest.xml | sed 's/<!--.*-->//g' | grep "<project" | sed 's/.*project //g' | grep 'remote="vanir"' | sed 's/[ ]*\/*>//g' | sed 's/groups=["a-zA-Z0-9,\-]*//g' | sed 's/.*path=\"//g' | sed 's/".*//g'`; do
     pathlist="$pathlist $x"
   done
-  echo $pathlist
   set +f
   [ ! "$pathlist" ] && echo "FAIL" && return 1
   repo forall -r $pathlist -c "$@"
