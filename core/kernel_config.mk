a1bc533edba2bfc2fc4050b8e30e3c3a399b1d44
nuclearmistake <nuclearmistake@gmail.com>
2014-07-23 20:25:26 -0400

    BONE_STOCK implies aosp kernel toolchains
    Change-Id: I8e1ef6f69c4be0b92ce257ad1fe75c7b8806f4fc

--- core/kernel_config.mk
+++ core/kernel_config.mk
@@ -4,14 +4,25 @@ ifeq ($(HOST_OS),darwin)
   TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
 endif
 
+ifneq ($(strip $(BONE_STOCK)),)
+  TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
+endif
+
 # sideways compat
 ifeq ($(USE_AOSP_TOOLCHAINS),true)
     TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
 endif
 
 # default
-ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
-    TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7
+ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7
+endif
+
+# mangling too support horendous nomenclature
+ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),linaro-4.9)
+    TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-eabi-
+else
+    TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-gnueabi-
 endif
 
 # allow setting the cpu variant for unsupported cpu's like cortex-a7
@@ -21,14 +32,6 @@ else
     TARGET_KERNEL_CPU_VARIANT := $(TARGET_KERNEL_CPU_VARIANT)
 endif
 
-
-# mangling too support horendous nomenclature
-ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),linaro-4.9)
-    TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-eabi-
-else
-    TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-gnueabi-
-endif
-
 # meat and potatoes
 ifeq ($(TARGET_KERNEL_USE_AOSP_TOOLCHAIN),true)
     ifneq ($(TARGET_KERNEL_CUSTOM_AOSP_TOOLCHAIN),)
@@ -36,6 +39,7 @@ ifeq ($(TARGET_KERNEL_USE_AOSP_TOOLCHAIN),true)
     else
         TARGET_KERNEL_CUSTOM_TOOLCHAIN:=arm-eabi-4.7
     endif
+
     TOOL_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-
 else
     T_K_C_T_STRIPPER := $(shell echo $(TARGET_KERNEL_CUSTOM_TOOLCHAIN) | sed -e 's/[a-z]//g')
