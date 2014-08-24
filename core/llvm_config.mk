3463063b846afd8c615b5bb498d54d5a2155791a
sudosurootdev <sudosurootdev@gmail.com>
2014-08-24 05:09:49 -0400

    subst mtune=cortex-a15 to krait2 for msm-% clang device... and march=armv7-a for prebuilt
    Change-Id: I6206057e0796ba4db46be4c0bf1a04ea3ade6ad0

--- core/llvm_config.mk
+++ core/llvm_config.mk
@@ -118,20 +118,17 @@ $(call clang-flags-subst,-Wno-psabi,)
 $(call clang-flags-subst,-Wno-unused-but-set-variable,)
 $(call clang-flags-subst,-Wno-unused-but-set-parameter,)
 
-# clang does not support cortex-a15, so FIRST (original)...
-# fall back to -march=armv7-a ...IF NO TARGET_CLANG_VERSION.
-ifeq ($(TARGET_CLANG_VERSION),)
-$(call clang-flags-subst,-mcpu=cortex-a15,-march=armv7-a)
-else
-# or use krait2 for krait cpu variants using msm-3.4 or msm-3.5 clang...
+# clang does not support cortex-a15 so use krait2 for msm clang...
 ifeq ($(filter-out msm%,$(TARGET_CLANG_VERSION)),)
-ifeq ($(TARGET_CPU_VARIANT),krait)
-$(call clang-flags-subst,-mtune=cortex-a15,-mtune=krait2)
-$(call clang-flags-subst,-mcpu=cortex-a15,-mcpu=krait2)
-$(call clang-flags-subst,-mtune=cortex-a9,-mtune=krait2)
-$(call clang-flags-subst,-mcpu=cortex-a9,-mcpu=krait2)
-endif
-endif
+  ifeq ($(TARGET_CPU_VARIANT),krait)
+    $(subst -mtune=cortex-a15,-mtune=krait2,$(TARGET_GLOBAL_CLANG_FLAGS))
+    $(subst -mcpu=cortex-a15,-mcpu=krait2,$(TARGET_GLOBAL_CLANG_FLAGS))
+    $(subst -mtune=cortex-a9,-mtune=krait2,$(TARGET_GLOBAL_CLANG_FLAGS))
+    $(subst -mcpu=cortex-a9,-mcpu=krait2,$(TARGET_GLOBAL_CLANG_FLAGS))
+  endif
+else
+# else fall back to -march=armv7-a ...IF NO TARGET_CLANG_VERSION.
+  $(subst %cortex-a15,-march=armv7-a,$(TARGET_GLOBAL_CLANG_FLAGS))
 endif
 
 ifneq ($(MAXIMUM_OVERDRIVE),true)
