b5f69dff95f57f86400094159c6262535e0cdc19
PrimeDirective <activethrasher00@gmail.com>
2014-08-27 16:43:20 -0400

    de-Bart clang header inclusion
    Change-Id: I28cb300d942e7240d0e8d39466aa5cfd277776e1

--- core/llvm_config.mk
+++ core/llvm_config.mk
@@ -94,11 +94,14 @@ ifeq ($(TARGET_ARCH),x86)
     -mbionic
 endif
 
-ifeq ($(TARGET_CLANG_VERSION),)
-CLANG_CONFIG_EXTRA_TARGET_C_INCLUDES := external/clang/lib/include $(TARGET_OUT_HEADERS)/clang
-else
-STOCK_CLANG_CONFIG_EXTRA_TARGET_C_INCLUDES := external/clang/lib/include $(TARGET_OUT_HEADERS)/clang
+ifneq ($(TARGET_CLANG_VERSION),)
+ifeq (,$(filter-out msm-%,$(TARGET_CLANG_VERSION)))
 CLANG_CONFIG_EXTRA_TARGET_C_INCLUDES := prebuilts/clang/$(BUILD_OS)-x86/host/$(TARGET_CLANG_VERSION)/lib/clang/$(TARGET_CLANG_VERSION)/include
+else
+$(error unknown TARGET_CLANG_VERSION specified - $(TARGET_CLANG_VERSION))
+endif
+else
+CLANG_CONFIG_EXTRA_TARGET_C_INCLUDES := external/clang/lib/include $(TARGET_OUT_HEADERS)/clang
 endif
 
 # remove unknown flags to define CLANG_FLAGS
