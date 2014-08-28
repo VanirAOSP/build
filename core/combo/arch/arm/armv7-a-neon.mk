966a5b095b1be550a257ad5f7857a86c0a8c02e0
nuclearmistake <nuclearmistake@gmail.com>
2014-08-28 10:12:12 -0400

    llvm_config: the derp that keeps on derp.
    Change-Id: Ic96de311ae1fd89af160ddab482401a117a81002

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -82,7 +82,7 @@ arch_variant_ldflags += \
 ## SNAPDRAGON CLANG/LLVM 3.4
 ######################################
 ifneq ($(TARGET_CLANG_VERSION),)
-ifeq ($(filter-out $(TARGET_CLANG_VERSION),msm-%),)
+ifeq ($(filter-out msm-%,$(TARGET_CLANG_VERSION)),)
 # krait specific clang optimizations
 ifeq ($(TARGET_CPU_VARIANT),krait)
 CLANG_MSM_EXTRA_CFLAGS += \
