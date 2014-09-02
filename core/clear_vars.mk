a8c689b6e55e11bf21e23cc41f310e921b4fce9c
PrimeDirective <activethrasher00@gmail.com>
2014-09-02 19:40:08 -0400

    clear_vars: add missing local defines for build flags we've added
    Change-Id: I6ea03d28980097b0f2446e7c1a9f9d003470102c

--- core/clear_vars.mk
+++ core/clear_vars.mk
@@ -142,6 +142,8 @@ LOCAL_POST_INSTALL_CMD:=
 LOCAL_DIST_BUNDLED_BINARIES:=
 LOCAL_HAL_STATIC_LIBRARIES:=
 LOCAL_DISABLE_GRAPHITE:=
+LOCAL_NO_LTO_SUPPORT:=
+LOCAL_STOCK_CLANG:=
 
 # Trim MAKEFILE_LIST so that $(call my-dir) doesn't need to
 # iterate over thousands of entries every time.
