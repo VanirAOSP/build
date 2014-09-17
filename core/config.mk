c518baf65447cbe8d202b6ecb7ed39f057c6b109
sudosurootdev <sudosurootdev@gmail.com>
2014-09-17 14:36:24 -0400

    Deal with CM changing vars (WITH_QC_PERF) for no reason, and still be able to use future changes where they use TARGET_HAVE_QC_PERF without difficulty.
    Change-Id: I46f8eb031f03e859561dae32401cebf328e05274

--- core/config.mk
+++ core/config.mk
@@ -163,6 +163,10 @@ endif
 TARGET_DEVICE_DIR := $(patsubst %/,%,$(dir $(board_config_mk)))
 board_config_mk :=
 
+ifeq ($(WITH_QC_PERF),true)
+  TARGET_HAVE_QC_PERF := true
+endif
+
 ## Rebuild the pathmap if there's a recovery variant. Its path probably changed
 ifneq ($(RECOVERY_VARIANT),)
 include $(BUILD_SYSTEM)/pathmap.mk
