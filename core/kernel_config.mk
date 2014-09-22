712ca6cecaf36d50369cd5537d79b5e771e09af4
DHOMD <DHO@dho.im>
2014-09-22 19:08:19 -0400

    Fix using linaro 4.8 toolchains for some devs
    Credit Lawrence Young
    
    Error:
    
    /home/giro/vanir/prebuilts/gcc/linux-x86/arm/linaro/linaro-4.8-cortex-a15/bin/arm-gnueabi-gcc: No such file or directory
    Is your PATH set correctly?
    
    Change-Id: Ibf746a076ad21555d1ad87e9303264f7ea78987b

--- core/kernel_config.mk
+++ core/kernel_config.mk
@@ -1,49 +1,55 @@
 # sweater-vest compat
 ifeq ($(HOST_OS),darwin)
-  MAKE_FLAGS := C_INCLUDE_PATH=$(ANDROID_BUILD_TOP)/external/elfutils/libelf
-  TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
+ MAKE_FLAGS := C_INCLUDE_PATH=$(ANDROID_BUILD_TOP)/external/elfutils/libelf
+ TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
 endif
 
 ifneq ($(strip $(BONE_STOCK)),)
-  TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
+ TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
 endif
 
 # sideways compat
 ifeq ($(USE_AOSP_TOOLCHAINS),true)
-    TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
+   TARGET_KERNEL_USE_AOSP_TOOLCHAIN := true
 endif
 
 # default
 ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
-    TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7
+   TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7
 endif
 
 # allow setting the cpu variant for unsupported cpu's like cortex-a7
 ifeq ($(TARGET_KERNEL_CPU_VARIANT),)
-    TARGET_KERNEL_CPU_VARIANT := $(cpu_for_optimizations)
+   TARGET_KERNEL_CPU_VARIANT := $(cpu_for_optimizations)
 else
-    TARGET_KERNEL_CPU_VARIANT := $(TARGET_KERNEL_CPU_VARIANT)
+   TARGET_KERNEL_CPU_VARIANT := $(TARGET_KERNEL_CPU_VARIANT)
 endif
 
 # meat and potatoes
 ifeq ($(TARGET_KERNEL_USE_AOSP_TOOLCHAIN),true)
-    ifneq ($(TARGET_KERNEL_CUSTOM_AOSP_TOOLCHAIN),)
-        TARGET_KERNEL_CUSTOM_TOOLCHAIN:=$(TARGET_KERNEL_CUSTOM_AOSP_TOOLCHAIN)
-    else
-        TARGET_KERNEL_CUSTOM_TOOLCHAIN:=arm-eabi-4.7
-    endif
+   ifneq ($(TARGET_KERNEL_CUSTOM_AOSP_TOOLCHAIN),)
+       TARGET_KERNEL_CUSTOM_TOOLCHAIN:=$(TARGET_KERNEL_CUSTOM_AOSP_TOOLCHAIN)
+   else
+       TARGET_KERNEL_CUSTOM_TOOLCHAIN:=arm-eabi-4.7
+   endif
 
-    TOOL_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-
+   TOOL_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-
 else
-    # mangling too support horendous nomenclature
-    ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),linaro-4.9)
-        TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-eabi-
-    else
-        TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-gnueabi-
-    endif
+   # mangling too support horendous nomenclature
+   ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),linaro-4.9)
+       TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-eabi-
+   else
+       TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-cortex_a15-linux-gnueabihf-
 
-    T_K_C_T_STRIPPER := $(shell echo $(TARGET_KERNEL_CUSTOM_TOOLCHAIN) | sed -e 's/[a-z]//g')
-    T_K_C_T_DASHER := $(shell echo $(T_K_C_T_STRIPPER) | sed -e 's/-//g')
-    T_K_C_T := linaro-$(T_K_C_T_DASHER)
-    TOOL_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/linaro/$(T_K_C_T)-$(TARGET_KERNEL_CPU_VARIANT)/bin/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS)
+   endif
+ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),4.8-linaro)
+       TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-cortex_a15-linux-gnueabihf-
+   else
+       TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS := arm-cortex_a15-linux-gnueabihf-
+
+   endif
+   T_K_C_T_STRIPPER := $(shell echo $(TARGET_KERNEL_CUSTOM_TOOLCHAIN) | sed -e 's/[a-z]//g')
+   T_K_C_T_DASHER := $(shell echo $(T_K_C_T_STRIPPER) | sed e 's///g')
+   T_K_C_T := linaro-$(T_K_C_T_DASHER)
+   TOOL_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/linaro/$(T_K_C_T)-$(TARGET_KERNEL_CPU_VARIANT)/bin/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN_ALIAS)
 endif
