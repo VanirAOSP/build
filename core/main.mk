7666b2f14c57979f039301a8933764867715faca
nuclearmistake <nuclearmistake@gmail.com>
2014-11-09 18:24:57 -0500

    Add missing endif, remove extra endif
    Change-Id: Ife3959a925e93564155932204480ebb12e2bae53

--- core/main.mk
+++ core/main.mk
@@ -46,6 +46,8 @@ $(warning *  You are using version $(MAKE_VERSION) of make.)
 $(warning *  Android can only be built by versions 3.81 and higher.)
 $(warning *  see https://source.android.com/source/download.html)
 $(warning ********************************************************************************)
+$(error stopping)
+endif
 endif
 
 # Absolute path of the present working direcotry.
