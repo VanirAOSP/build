c2a9fbcc7847f3a5aa11fd443a8b511fd2adbfe3
Dave Kessler <activethrasher00@gmail.com>
2014-12-14 01:42:31 -0500

    Squashed update of vanir_config
    Revert "vanir_config v1.0.1: introduce a centralized location for custom build flags"
    
    This reverts commit e0e0adae8d565df6ae4f4aea67c52187d24079a4.
    
    vanir_config v2.0: the correct version of vanir_config.mk
    
    The prime directive of this config is to provide a simple centralized
    interface to allow different custom build improvements and configurations
    with respect to their set status in device trees.
    
    Revisions should respect both Clang and GCC compilers for compatibility.
    
    current build configurations:
    BONE_STOCK := set true to override all vanir_config variables
    NO_DEBUG_SYMBOL_FLAGS := true removes debugging code insertions from assert.h macros and GDB
    MAXIMUM_OVERDRIVE := true disables address sanitizer, set in $(BUILD_SYSTEM)/core/clang/config.mk
    USE_GRAPHITE := true adds graphite cflags to turn on graphite
    USE_FSTRICT_FLAGS := true builds with fstrict-aliasing (thumb and arm)
    FSTRICT_ALIASING_WARNING_LEVEL := 0-3 for what is considered an aliasing violation
    
    PS2: import only in config.mk and give values set in device trees priority
    PS3: whitespace
    PS9: Hope this helps.
    PS12: Ensure that clang flags (set before the clang target makefiles would stomp on them) are set
          Set same clang flags for arm64
    PS14: use cflag precedence to handle default thumb -fno-strict-aliasing
    PS15: arm and thumb cflags have -fomit-frame-pointer already
          remove newline before fstrict flags so TARGET_linux-arm cflags don't cause ccache misses if no optional flags are set
    PS17: get rid of unnecessary patchset changes
    
    Change-Id: Ifa8d5eb454c57d14322d170832d89da40ad6f115
    Signed-off-by: DHOMD <DHO@dho.im>
    Conflicts:
    	core/clang/config.mk
    
    MAXIMUM_OVERDRIVE := true
    
    This tested to bench better and regular vanir source will take care of
    any stability issues so this should be safe to use
    
    Change-Id: I83302d3aeacb09a4bc105a4194ef914fbaa25317
    
    vanir_config.mk:  Add clang/GCC specific flag options
    
    Change-Id: I4d37140c3b393ef3fc9b2235bea51daf3b3d1105
    
    vanir_config 2.1
    
    optimization level flexibility
    temp workaround handling for fstrict aliasing
    add VANIR_GLOBAL_CPPFLAGS
    add handling of output flags to select.mk
    
    Change-Id: Ia89a608d726b47e28118d59db8142dcc57bd8787
    
    vanir_config: preliminary global fstrict-aliasing
    
    Change-Id: Ifc96c5e526cff1be6e0a8395ca63f4a8ab3b55bb
    
    vanir_config: add support for link-time optimization per module
    
    BOOM. headshot.
    
    Change-Id: I451f88937c7c920f62d156fba94c2681e851a697
    
    vanir_config: add libOmxVenc to VANIR_FNO_STRICT_ALIASING_MODULES
    
    Change-Id: I594be805769e65858a829bcbdfaad2310a3b4816
    
    vanir_config: remove some clingon LTO modules that got merged by accident
    
    Change-Id: If840f1831569101663cbb08b6b99698b5965f302
    
    vanir_config: update license date
    
    Change-Id: Id2460707b6046f13535ad1a7a01f75fdd8d25b1b

--- core/vanir_config.mk
+++ core/vanir_config.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 VanirAOSP && The Android Open Source Project
+# Copyright (C) 2015 Exodus/Vanir
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,9 +26,34 @@
 # MAXIMUM_OVERDRIVE := true disables address sanitizer, in core/clang/config.mk
 # USE_GRAPHITE := true adds graphite cflags to turn on graphite
 # USE_FSTRICT_FLAGS := true builds with fstrict-aliasing (thumb and arm)
-# FSTRICT_ALIASING_WARNING_LEVEL := 0-3 for what is considered an aliasing violation
+# USE_BINARY_FLAGS := true adds experimental binary flags that can be set here or in device trees
+# USE_EXTRA_CLANG_FLAGS := true allows additional flags to be passed to the Clang compiler
+# ADDITIONAL_TARGET_ARM_OPT := Additional flags may be appended here for GCC-specific modules, -O3 etc
+# ADDITIONAL_TARGET_THUMB_OPT := Additional flags may be appended here for GCC-specific modules, -O3 etc
+# VANIR_ARM_OPT_LEVEL := -Ox for TARGET_arm_CFLAGS, preserved in binary.mk
+# VANIR_THUMB_OPT_LEVEL := -Ox for TARGET_thumb_CFLAGS, preserved in binary.mk
+# FSTRICT_ALIASING_WARNING_LEVEL := 0-3 for the level of intensity the compiler checks for violations.
+# USE_LTO := true builds the listed modules with the -flto flags
 
+# SET GLOBAL CONFIGURATION HERE:
+MAXIMUM_OVERDRIVE           ?= true
+NO_DEBUG_SYMBOL_FLAGS       ?= true
+NO_DEBUG_FRAME_POINTERS     ?= true
+USE_GRAPHITE                ?=
+USE_LTO                     ?= true
+USE_FSTRICT_FLAGS           ?= true
+USE_BINARY_FLAGS            ?=
+USE_EXTRA_CLANG_FLAGS       ?=
+ADDITIONAL_TARGET_ARM_OPT   ?=
+ADDITIONAL_TARGET_THUMB_OPT ?=
+VANIR_ARM_OPT_LEVEL         ?= -O2
+VANIR_THUMB_OPT_LEVEL       ?= -Os
+FSTRICT_ALIASING_WARNING_LEVEL ?= 2
+
+# Set some defaults in case they are missing
 ifeq ($(FSTRICT_ALIASING_WARNING_LEVEL),)
+  VANIR_ARM_OPT_LEVEL         ?= -O2
+  VANIR_THUMB_OPT_LEVEL       ?= -Os
   FSTRICT_ALIASING_WARNING_LEVEL := 2
 endif
 
@@ -37,11 +63,20 @@ ifeq ($(BONE_STOCK),true)
   NO_DEBUG_SYMBOL_FLAGS   :=
   USE_GRAPHITE            :=
   USE_FSTRICT_FLAGS       :=
+  USE_BINARY_FLAGS        :=
+  USE_EXTRA_CLANG_FLAGS   :=
+  VANIR_ARM_OPT_LEVEL     := -O2
+  VANIR_THUMB_OPT_LEVEL   := -Os
+  ADDITIONAL_TARGET_ARM_OPT   :=
+  ADDITIONAL_TARGET_THUMB_OPT :=
 endif
 
 # DEBUGGING OPTIONS
 ifeq ($(NO_DEBUG_SYMBOL_FLAGS),true)
-  DEBUG_SYMBOL_FLAGS := -g0
+  DEBUG_SYMBOL_FLAGS := -g0 -DNDEBUG
+endif
+ifeq ($(NO_DEBUG_FRAME_POINTERS),true)
+  DEBUG_FRAME_POINTER_FLAGS := -fomit-frame-pointer
 endif
 
 # GRAPHITE
@@ -56,16 +91,157 @@ ifeq ($(USE_GRAPHITE),true)
           -floop-block
 endif
 
+# Assign modules to build with link time optimizations using VANIR_LTO_MODULES.
+ifeq ($(USE_LTO),true)
+  VANIR_LTO_MODULES := \
+    core.art \
+    libart \
+    libart-compiler \
+    libartd \
+    libartd-compiler \
+    libart-disassembler \
+    libartd-disassembler \
+    libsigchain \
+    dalvikvm \
+    dalvikvm32 \
+    dalvikvm64 \
+    libart-gtest \
+    libegl \
+    egl.cfg \
+    libGLESv2 \
+    libGLESv1_CM
+
+  VANIR_LTO_FLAGS := \
+    -flto \
+    -fuse-linker-plugin \
+    $(DEBUG_SYMBOL_FLAGS)
+endif
+
+# fstrict-aliasing. Thumb is defaulted off for AOSP. Use VANIR_SPECIAL_CASE_MODULES to
+# temporarily disable fstrict-aliasing locally in modules we dont care about or until the
+# error it contains is properly fixed.
 ifeq ($(USE_FSTRICT_FLAGS),true)
+  VANIR_FNO_STRICT_ALIASING_MODULES := \
+	libc_bionic \
+    libandroid_runtime \
+    libziparchive-host \
+	libc_dns \
+	libc_tzcode \
+	libc_openbsd \
+	libc \
+	logd \
+	mdnsd \
+	libziparchive \
+	libdiskconfig \
+	libtwrpmtp \
+	libfusetwrp \
+	libguitwrp \
+	busybox \
+	static_busybox \
+	libuclibcrpc \
+	ping \
+	ping6 \
+	libjavacore \
+	libfdlibm \
+	libvariablespeed \
+	librtp_jni \
+	libdownmix \
+	libldnhncr \
+	libqcomvisualizer \
+	libwilhelm \
+	libvisualizer \
+	libstagefright \
+	libstagefright_webm \
+	libmedia \
+	libreverb \
+	libaudioflinger \
+	libmediaplayerservice \
+	libstagefright_soft_h264dec \
+	libmusicbundle \
+	libstlport \
+	libstlport_static \
+	libutils \
+	libandroidfw \
+	dnsmasq \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support \
+	content_content_renderer_gyp \
+	net_net_gyp \
+	third_party_WebKit_Source_modules_modules_gyp \
+	third_party_WebKit_Source_platform_blink_platform_gyp \
+	third_party_WebKit_Source_core_webcore_remaining_gyp \
+	third_party_angle_src_translator_lib_gyp \
+	third_party_WebKit_Source_core_webcore_generated_gyp \
+	libc_gdtoa \
+	libc_nomalloc \
+	libft2 \
+	libjni_jpegstream \
+	libjni_filtershow_filters \
+	libjni_jpegutil \
+	gatt_testtool \
+	bluetooth.default \
+	sensors.$(TARGET_BOOTLOADER_BOARD_NAME) \
+	libnvvisualizer \
+	libnfc-nci \
+	libssh \
+	ssh \
+	libOmxVdec \
+	libOmxVenc \
+    mm-vdec-omx-test \
+    libpdfiumcore \
+    libpdfium
+
+# external/ffmpeg
+  VANIR_FNO_STRICT_ALIASING_MODULES += \
+	libavcodec \
+	libavutil \
+	libavformat \
+	libswscale
+
   FSTRICT_FLAGS := \
           -fstrict-aliasing \
           -Wstrict-aliasing=$(FSTRICT_ALIASING_WARNING_LEVEL) \
           -Werror=strict-aliasing
-  VANIR_ARM_FSTRICT_OPTIONS := $(FSTRICT_FLAGS)
-  VANIR_THUMB_FSTRICT_OPTIONS := $(FSTRICT_FLAGS)
 endif
 
-VANIR_GLOBAL_CFLAGS += $(GRAPHITE_FLAGS)
-VANIR_RELEASE_CFLAGS += $(DEBUG_SYMBOL_FLAGS)
-VANIR_CLANG_TARGET_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS)
+# Additional GCC-specific arm cflags
+ifeq ($(ADDITIONAL_TARGET_ARM_OPT),true)
+    VANIR_TARGET_ARM_FLAGS := \
+        -ftree-vectorize \
+        -funsafe-loop-optimizations
+endif
 
+# Additional GCC-specific thumb cflags
+ifeq ($(ADDITIONAL_TARGET_THUMB_OPT),true)
+    VANIR_TARGET_THUMB_FLAGS := \
+        -funsafe-math-optimizations
+endif
+
+# Additional clang-specific cflags
+ifeq ($(USE_EXTRA_CLANG_FLAGS),true)
+    VANIR_CLANG_CONFIG_EXTRA_ASFLAGS :=
+    VANIR_CLANG_CONFIG_EXTRA_CFLAGS :=
+    VANIR_CLANG_CONFIG_EXTRA_CPPFLAGS :=
+    VANIR_CLANG_CONFIG_EXTRA_LDFLAGS :=
+endif
+
+#======================================================================================================
+# variables as exported to other makefiles ============================================================
+VANIR_FSTRICT_OPTIONS := $(FSTRICT_FLAGS)
+
+VANIR_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
+VANIR_RELEASE_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
+VANIR_CLANG_TARGET_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
+VANIR_GLOBAL_CPPFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
+
+# set experimental/unsupported flags here for persistance and try to override local options that
+# may be set after release flags.  This option should not be used to set flags globally that are
+# intended for release but to test outcomes.  For example: setting -O3 here will have a higher
+# likelyhood of overriding the stock and local flags.
+ifdef ($(USE_BINARY_FLAGS),true)
+VANIR_BINARY_CFLAG_OPTIONS := $(GRAPHITE_FLAGS)
+VANIR_BINARY_CPP_OPTIONS := $(GRAPHITE_FLAGS)
+VANIR_LINKER_OPTIONS :=
+VANIR_ASSEMBLER_OPTIONS :=
+endif
