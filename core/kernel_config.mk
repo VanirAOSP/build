730e857098b33ff8171b11babcaeb83b060da3e7
sudosurootdev <sudosurootdev@gmail.com>
2015-02-04 02:49:05 -0500

    Prepare kernel_config.mk for linaro-4.10 which only has generic
    Change-Id: Id53ec6ab9fe7672f1d2771f9e17a48e035fb087a

--- core/kernel_config.mk
+++ core/kernel_config.mk
@@ -22,18 +22,22 @@ else # after target stuff set, set real values
         # and TARGET_KERNEL_TOOLCHAIN_VERSION is prefixed with linaro-
         ifneq ($(findstring linaro-,$(TARGET_KERNEL_TOOLCHAIN_VERSION)),)
             # figure out cpu variant to use the specific toolchain for
-            ifeq ($(TARGET_KERNEL_CPU_VARIANT),)
-                ifeq ($(TARGET_CPU_VARIANT),krait)
-                    TARGET_KERNEL_CPU_VARIANT := cortex-a15
-                else
-                    ifeq ($(wildcard $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/linaro/$(T_K_C_T)-$(TARGET_CPU_VARIANT)),)
-                        # no specific toolchain folder exists, use generic
-                        TARGET_KERNEL_CPU_VARIANT := generic
-                        $(warn Could not find specific $(TARGET_KERNEL_TOOLCHAIN_VERSION) toolchain for $(TARGET_CPU_VARIANT))
+            ifneq ($(TARGET_KERNEL_TOOLCHAIN_VERSION),linaro-4.10)
+                ifeq ($(TARGET_KERNEL_CPU_VARIANT),)
+                    ifeq ($(TARGET_CPU_VARIANT),krait)
+                        TARGET_KERNEL_CPU_VARIANT := cortex-a15
                     else
-                        TARGET_KERNEL_CPU_VARIANT := $(TARGET_CPU_VARIANT)
+                        ifeq ($(wildcard $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/linaro/$(T_K_C_T)-$(TARGET_CPU_VARIANT)),)
+                            # no specific toolchain folder exists, use generic
+                            TARGET_KERNEL_CPU_VARIANT := generic
+                            $(warn Could not find specific $(TARGET_KERNEL_TOOLCHAIN_VERSION) toolchain for $(TARGET_CPU_VARIANT))
+                        else
+                            TARGET_KERNEL_CPU_VARIANT := $(TARGET_CPU_VARIANT)
+                        endif
                     endif
                 endif
+            else
+                TARGET_KERNEL_CPU_VARIANT := generic
             endif
             T_K_C_T_STRIPPER := $(shell echo $(TARGET_KERNEL_TOOLCHAIN_VERSION) | sed -e 's/[a-z]//g')
             T_K_C_T_DASHER := $(shell echo $(T_K_C_T_STRIPPER) | sed -e 's/-//g')
