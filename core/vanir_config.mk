35dbd1d2795165b6cf397465eefef442e8e8a434
Ethan <cannondalev2000@gmail.com>
2015-04-11 22:55:05 -0700

    Remove graphite from vanir_config since it is never used and replace it with pipe option instead.
    pipe is known to shorten build times by 5-10 minutes on machines with conventional spiner drives.
    Gains on ssd drive are not nearly as much if any
    
    PS2: cleanup white space, remove extra endif and remove graphite discription in favor or pipe discription
    
    Change-Id: I6da106d400da57bb9b77ed1d9ca0d00bf2251605

--- core/vanir_config.mk
+++ core/vanir_config.mk
@@ -24,7 +24,7 @@
 # NO_DEBUG_FRAME_POINTERS := set true to add frame pointers
 # NO_DEBUG_SYMBOL_FLAGS := true removes debugging code insertions from assert.h macros and GDB
 # MAXIMUM_OVERDRIVE := true disables address sanitizer, in core/clang/config.mk
-# USE_GRAPHITE := true adds graphite cflags to turn on graphite
+# USE_PIPE := true enables -pipe option to speed up build times in some cases
 # USE_FSTRICT_FLAGS := true builds with fstrict-aliasing (thumb and arm)
 # USE_BINARY_FLAGS := true adds experimental binary flags that can be set here or in device trees
 # USE_EXTRA_CLANG_FLAGS := true allows additional flags to be passed to the Clang compiler
@@ -72,17 +72,10 @@ ifeq ($(NO_DEBUG_FRAME_POINTERS),true)
   DEBUG_FRAME_POINTER_FLAGS := -fomit-frame-pointer
 endif
 
-# GRAPHITE
-ifeq ($(USE_GRAPHITE),true)
-  GRAPHITE_FLAGS := \
-          -fgraphite             \
-          -floop-flatten         \
-          -floop-parallelize-all \
-          -ftree-loop-linear     \
-          -floop-interchange     \
-          -floop-strip-mine      \
-          -floop-block
-endif
+# PIPE
+ifeq ($(USE_PIPE),true)
+  PIPE_FLAGS := \
+    -pipe
 
 # Assign modules to build with link time optimizations using VANIR_LTO_MODULES.
 ifeq ($(USE_LTO),true)
@@ -206,18 +199,18 @@ endif
 # variables as exported to other makefiles ============================================================
 VANIR_FSTRICT_OPTIONS := $(FSTRICT_FLAGS)
 
-VANIR_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
-VANIR_RELEASE_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
-VANIR_CLANG_TARGET_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
-VANIR_GLOBAL_CPPFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)
+VANIR_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS) $(PIPE_FLAGS)
+VANIR_RELEASE_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS) $(PIPE_FLAGS)
+VANIR_CLANG_TARGET_GLOBAL_CFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS)$(PIPE_FLAGS)
+VANIR_GLOBAL_CPPFLAGS += $(DEBUG_SYMBOL_FLAGS) $(DEBUG_FRAME_POINTER_FLAGS) $(PIPE_FLAGS)
 
 # set experimental/unsupported flags here for persistance and try to override local options that
 # may be set after release flags.  This option should not be used to set flags globally that are
 # intended for release but to test outcomes.  For example: setting -O3 here will have a higher
 # likelyhood of overriding the stock and local flags.
 ifdef ($(USE_BINARY_FLAGS),true)
-VANIR_BINARY_CFLAG_OPTIONS := $(GRAPHITE_FLAGS)
-VANIR_BINARY_CPP_OPTIONS := $(GRAPHITE_FLAGS)
+VANIR_BINARY_CFLAG_OPTIONS :=
+VANIR_BINARY_CPP_OPTIONS :=
 VANIR_LINKER_OPTIONS :=
 VANIR_ASSEMBLER_OPTIONS :=
 endif
