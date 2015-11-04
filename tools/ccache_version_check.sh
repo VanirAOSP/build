94c816bba09dbf5d9d6690f601fc7bf9ea4cd9c8
nuclearmistake <nuclearmistake@gmail.com>
2015-11-04 08:02:01 -0500

    fix erroneous newlines in get_build_var output
    caused by regression in inline ccache compilation
    
    Change-Id: I116cafbb56e0f7857268e06f659a5e5988c7de89

--- tools/ccache_version_check.sh
+++ tools/ccache_version_check.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' autogen|grep "install ok installed")
-echo Checking for autogen: $PKG_OK
-if [ "" == "$PKG_OK" ]; then
-  echo "autogen is required to build ccache and was not found."
-  echo "installing autogen..."
-  sudo apt-get --force-yes --yes install autogen
+if [ "`which automake``which autogen`" = "" ]; then
+  echo "automake or autogen is required to build ccache and was not found." 1>&2
+  if [ "`which apt-get`" != "" ]; then
+    echo "INSTALLING AUTOMAKE. You will now be asked to approve sudo" 1>&2
+    sudo bash -c "apt-get install -y --force-yes automake 1>&2"
+  else
+    for x in {1..10}; do
+      echo "As you don't have apt-get, you must install automake or autogen manually" 1>&2
+    done
+    exit 1
+  fi
 fi
 
 if [ -e ./ccache/ccache ] && grep $(./ccache/ccache -V | grep "ccache version" | awk '{print $3}') ccache/version.c 2>&1 >/dev/null; then
@@ -19,3 +24,5 @@ else
     popd 2>&1 >/dev/null
     echo "ccache updated to version `ccache/ccache -V`" 1>&2
 fi
+mkdir -p `pwd`/.ccachesymlinks
+$(dirname $0)/update-ccache-symlinks `pwd`/.ccachesymlinks
