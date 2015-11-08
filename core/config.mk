d269857916ac87ae9d085c08a8968de224488bb6
nuclearmistake <nuclearmistake@gmail.com>
2015-11-08 14:22:56 -0500

    Break CM_BUILD conditionalization with recless abandon
    Change-Id: I82cfb97b53df5d0b9b1ceb8d69a051de97aa1f83

--- core/config.mk
+++ core/config.mk
@@ -911,7 +911,7 @@ ifneq ($(TARGET_COPY_FILES_OVERRIDES),)
     PRODUCT_COPY_FILES := $(filter-out $(TARGET_COPY_FILES_OVERRIDES), $(PRODUCT_COPY_FILES))
 endif
 
-ifneq ($(CM_BUILD),)
+#ifneq ($(CM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/vanir/sepolicy/sepolicy.mk)
@@ -922,6 +922,6 @@ $(eval include vendor/vanir/sepolicy/sepolicy.mk)
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
-endif
+#endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
