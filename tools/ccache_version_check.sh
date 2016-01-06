afdd4596ff329bdf9751feb37ab1ef7efb164826
nuclearmistake <nuclearmistake@gmail.com>
2016-01-06 01:22:36 -0500

    Fix ccache_version_check.sh -- version.c is generated
    Change-Id: I12ca67e68625a0c5d73c8b8ad2be0783c8d00721

--- tools/ccache_version_check.sh
+++ tools/ccache_version_check.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+get_source_ccache_version()
+{
+   version=`git --git-dir=./ccache/.git describe --dirty 2>/dev/null || echo unknown`
+   if echo $version | grep -q "^v"; then
+     version="`echo $version | sed 's/v//'`"
+   fi
+   echo $version | sed -e 's/-/+/' -e 's/-/_/g'
+}
+
 if [ "`which automake``which autogen`" = "" ]; then
   echo "automake or autogen is required to build ccache and was not found." 1>&2
   if [ "`which apt-get`" != "" ]; then
@@ -11,16 +21,18 @@ if [ "`which automake``which autogen`" = "" ]; then
     exit 1
   fi
 fi
-
-if [ -e ./ccache/ccache ] && grep $(./ccache/ccache -V | grep "ccache version" | awk '{print $3}') ccache/version.c 2>&1 >/dev/null; then
+current_version="`get_source_ccache_version`"
+if [ -e ./ccache/ccache ] && echo $current_version | grep -q $(./ccache/ccache -V | grep "ccache version" | awk '{print $3}'); then
     # ccache exists and its --version output matches the latest source
     echo "ccache is up to date" 1>&2
 else
+    set -e
     echo "building ccache binary" 1>&2
     pushd ccache 2>&1 >/dev/null
-    ./autogen.sh 1>&2
-    ./configure 1>&2
-    make 1>&2
+    ./autogen.sh >/dev/null
+    ./configure >/dev/null
+    make >/dev/null
     popd 2>&1 >/dev/null
-    echo "ccache updated to version `ccache/ccache -V`" 1>&2
+    set +e
+    echo "ccache updated to version $current_version" 1>&2
 fi
