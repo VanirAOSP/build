bc3261c13fbcdc082d62a74322555111ff6b67c6
nuclearmistake <nuclearmistake@gmail.com>
2016-08-31 16:49:17 -0400

    Remove cm-y cmremote function
    Change-Id: I78fea15118cc6acb9e9aca0d8af6fbef7a780de6

--- envsetup.sh
+++ envsetup.sh
@@ -1795,25 +1795,6 @@ function godir () {
     \cd $T/$pathname
 }
 
-function cmremote()
-{
-    if ! git rev-parse --git-dir &> /dev/null
-    then
-        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
-        return 1
-    fi
-    git remote rm cmremote 2> /dev/null
-    GERRIT_REMOTE=$(git config --get remote.github.projectname)
-    CMUSER=$(git config --get review.review.cyanogenmod.org.username)
-    if [ -z "$CMUSER" ]
-    then
-        git remote add cmremote ssh://review.cyanogenmod.org:29418/$GERRIT_REMOTE
-    else
-        git remote add cmremote ssh://$CMUSER@review.cyanogenmod.org:29418/$GERRIT_REMOTE
-    fi
-    echo "Remote 'cmremote' created"
-}
-
 function aospremote()
 {
     if ! git rev-parse --git-dir &> /dev/null
