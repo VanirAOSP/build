LineageOS/android_build

Move CM build additions to vendor/cm/build
Change-Id: I39d3e711a6beab47e3284919a9a4fef15b0f40b9

core/config.mk
@@ -566,10 +566,10 @@ else
 endif
 
 # Rules for QCOM targets
-include $(BUILD_SYSTEM)/qcom_target.mk
+include vendor/cm/build/core/qcom_target.mk
 
 # Rules for MTK targets
-include $(BUILD_SYSTEM)/mtk_target.mk
+include vendor/cm/build/core/mtk_target.mk
 
 # ###############################################################
 # Set up final options.

core/main.mk
@@ -106,10 +106,10 @@ include $(BUILD_SYSTEM)/config.mk
 include $(BUILD_SYSTEM)/cleanbuild.mk
 
 # Bring in Qualcomm helper macros
-include $(BUILD_SYSTEM)/qcom_utils.mk
+include vendor/cm/build/core/qcom_utils.mk
 
 # Bring in Mediatek helper macros too
-include $(BUILD_SYSTEM)/mtk_utils.mk
+include vendor/cm/build/core/mtk_utils.mk
 
 # Include the google-specific config
 -include vendor/google/build/config.mk

VanirAOSP/build

core/config.mk
@@ -659,10 +659,10 @@ else
 endif
 
 # Rules for QCOM targets
-include $(BUILD_SYSTEM)/qcom_target.mk
+include vendor/vanir/build/core/qcom_target.mk
 
 # Rules for MTK targets
-include $(BUILD_SYSTEM)/mtk_target.mk
+include vendor/vanir/build/core/mtk_target.mk
 
 # ###############################################################
 # Set up final options.

core/main.mk
@@ -146,10 +146,10 @@ endif
 include $(BUILD_SYSTEM)/cleanbuild.mk
 
 # Bring in Qualcomm helper macros
-include $(BUILD_SYSTEM)/qcom_utils.mk
+include vendor/vanir/build/core/qcom_utils.mk
 
 # Bring in Mediatek helper macros too
-include $(BUILD_SYSTEM)/mtk_utils.mk
+include vendor/vanir/build/core/mtk_utils.mk
 
 # Include the google-specific config
 -include vendor/google/build/config.mk