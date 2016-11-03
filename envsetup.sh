d802c3ce2968f2e1d30874fde329addeb4cdcdd0
nuclearmistake <nuclearmistake@gmail.com>
2016-11-03 07:07:51 -0400

    Move bottleservice giblets to env befote champagning
    Change-Id: I93ca22795f24032ebeb8358cc9aa8c2ce5b1588b

--- envsetup.sh
+++ envsetup.sh
@@ -647,6 +647,9 @@ function lunch()
         pushd . >& /dev/null
         cd $T
         local TARGET_PREBUILT_KERNEL=$(get_build_var TARGET_PREBUILT_KERNEL)
+        export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
+        export TARGET_LERNEL_SOURCE=$(get_build_var TARGET_KERNEL_SOURCE)
+        export TARGET_NO_KERNEL=$(get_build_var TARGET_NO_KERNEL) 
         if [ ! $TARGET_PREBUILT_KERNEL ] || [ "$TARGET_PREBUILT_KERNEL" = "false" ]; then
             source $T/vendor/vanir/build/tools/bottleservice.sh
             champagne $product || return 1
