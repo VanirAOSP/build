1f67b51f8c69b5ce204eb22ae1db08a0d4bbfb15
DHOMD <DHO@dho.im>
2017-10-05 20:33:15 -0400

    Let's use LNG's device/sepolicy
    Change-Id: I62ded48dcc917c2a5d9d5e5841c85f2aaebe8ef4

--- core/config.mk
+++ core/config.mk
@@ -926,7 +926,7 @@ endif
 ifneq ($(VANIR_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/vanir/sepolicy/common/sepolicy.mk)
+$(eval include device/lineage/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
